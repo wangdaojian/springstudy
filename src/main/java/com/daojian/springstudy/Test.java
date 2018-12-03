c6f94dcb22f68b55adc9aee25dea6d0634ef27c1
wangdaojian <wangdaojian@yazuo.com>
2018-12-03 18:44:10 +0800

    rdd

--- src/main/java/com/daojian/springstudy/Test.java
+++ src/main/java/com/daojian/springstudy/Test.java
@@ -6,6 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 	
 	public static void main(String[] args) {
-		ApplicationContext act = new ClassPathXmlApplicationContext()
+		ApplicationContext act = new ClassPathXmlApplicationContext("application.xml");
+		Hello hl = (Hello)act.getBean("hello");
+		System.out.println(hl);
+		hl.hello();
 	}
 }
